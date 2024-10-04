c2be6a2413d3fc6276c489da2cf04866e07ddb23
gracexuwt <gracexuwt1126@gmail.com>
2024-10-04 19:23:41 -0400

    Player scene

---
+++ scripts/characters/player.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+@export var movement_speed = 200
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * movement_speed
+		if(velocity.x != 0 && velocity.y != 0):
+			get_node("AnimatedSprite2D").stop()
+		elif velocity.x > 0:
+			get_node("AnimatedSprite2D").play("walk_right")
+		elif velocity.x < 0:
+			get_node("AnimatedSprite2D").play("walk_left")
+		elif velocity.y > 0:
+			get_node("AnimatedSprite2D").play("walk_up")
+		else:
+			get_node("AnimatedSprite2D").play("walk_down")
+	else:
+		get_node("AnimatedSprite2D").stop()
+		
+	# Update position
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
