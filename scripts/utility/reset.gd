fbca4e6c8092dc13f2ad4d8b921a44827d076685
gracexuwt <gracexuwt1126@gmail.com>
2024-10-06 02:00:37 -0400

    Add new tiles

--- scripts/utility/reset.gd
+++ scripts/utility/reset.gd
@@ -13,13 +13,13 @@ func _on_body_entered(body: Node2D) -> void:
 		for b in original_positions.keys():
 			if b:
 				b.global_position = original_positions[b]
-		var image = Image.load_from_file("res://assets/art/objects/resetted.png")
+		var image = Image.load_from_file("res://resources/tiles/button.png")
 		var texture = ImageTexture.create_from_image(image)
 		get_node("Sprite2D").texture = texture
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		var image = Image.load_from_file("res://assets/art/objects/reset.png")
+		var image = Image.load_from_file("res://resources/tiles/button(1).png")
 		var texture = ImageTexture.create_from_image(image)
 		get_node("Sprite2D").texture = texture
